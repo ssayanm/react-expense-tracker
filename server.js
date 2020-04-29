5c9f97bd4f8d65218a61202f387dc102b1bb0e89
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-04-29 11:00:49 +0530

    created the mongodb database and coonected to the server.js

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
 
 dotenv.config({ path: "./config/.config.env" });
+
+connectDB();
 
 const transactions = require("./routes/transactions");
 
@@ -15,6 +18,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on ${PORT}`.red.bold
+    `Server is running in ${process.env.NODE_ENV} mode on ${PORT}`.yellow.bold
   )
 );
