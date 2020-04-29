1658219447b62779b2d9adc6b4c2aed4abaa3f4e
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-04-29 10:40:28 +0530

    created all the get, post and delete controllers and routes

---
+++ controllers/transactionsControllers.js
@@ -0,0 +1,23 @@
+//@desc     Get all the transactions
+//@route    /api/v1/transactions
+//@access   Public
+
+exports.getTransactions = (req, res, next) => {
+  res.send("GET transactions");
+};
+
+//@desc     Add transaction
+//@route    POST /api/v1/transactions
+//@access   Public
+
+exports.addTransaction = (req, res, next) => {
+  res.send("POST transactions");
+};
+
+//@desc     Delete transaction
+//@route    DELETE /api/v1/transactions/:id
+//@access   Public
+
+exports.deleteTransaction = (req, res, next) => {
+  res.send("DELETE transactions");
+};
