5bceb656c99f49e4039d199139af882ad01e8eeb
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-11-10 11:11:32 +0530

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,7 @@ import Balance from "./components/Balance";
 import IncomeExpenses from "./components/IncomeExpenses";
 import TransactionList from "./components/TransactionList";
 import AddTransaction from "./components/AddTransaction";
-
 import { GlobalProvider } from "./context/GlobalState";
-
 import "./App.css";
 
 function App() {
